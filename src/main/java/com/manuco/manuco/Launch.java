7843c589e7d868a1591a4cd7c82d3164bcadf42c
Drew Brokamp <drew.brokamp@gmail.comm>
2022-05-01 19:06:03 -0400

    Renamed Main to Launch for clarification

--- src/main/java/com/manuco/manuco/Launch.java
+++ src/main/java/com/manuco/manuco/Launch.java
@@ -2,28 +2,22 @@ package com.manuco.manuco;
 
 import com.manuco.manuco.Controller.SceneController;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * Application launch point
  */
 
-public class Main extends Application {
+public class Launch extends Application {
 
     SceneController sceneController = SceneController.getSceneControllerInstance();
 
     /**
      * Launch point for JavaFX; creates main stage
      * @param stage - the top level JavaFX container for all scenes
-     * @throws IOException - FXMLLoader.load throws I/O if fxml file is not found.
      */
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage)  {
 
         sceneController.setInitialStageAndScene(stage);
 
