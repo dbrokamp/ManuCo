2cd82fd7fae96e299ef93b9a970cddd2f6dae86c
Drew Brokamp <drew.brokamp@gmail.comm>
2022-05-01 18:40:01 -0400

    Added setInitialStageAndScene method to SceneController

--- src/main/java/com/manuco/manuco/Controller/SceneController.java
+++ src/main/java/com/manuco/manuco/Controller/SceneController.java
@@ -8,29 +8,48 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * StageController implements the Singleton pattern to ensure only one instance is created and used to manage
+ * SceneController implements the Singleton pattern to ensure only one instance is created and used to manage
  * the scene currently presented to the user.
  */
 
-public class StageController {
+public class SceneController {
 
-    private static StageController instance;
+    /**
+     * Create single instance of SceneController object
+     */
+    private static SceneController instance = new SceneController();
 
 
     /**
      * Protected constructor to ensure only one instance is created.
      */
-    protected StageController() {}
+    protected SceneController() {}
 
     /**
-     * Gets the single instance of StageController or creates one if the single instance is null.
-     * @return instance - returns the single instance of the StageController.
+     * Gets the single instance of SceneController or creates one if the single instance is null.
+     * @return instance - returns the single instance of the SceneController.
      */
-    public static StageController getStageManagerInstance() {
+    public static SceneController getSceneControllerInstance() {
         if (instance == null) {
-            instance = new StageController();
+            instance = new SceneController();
         }
         return instance;
+    }
+
+    /**
+     * Creates the top level stage and displays the MainView.
+     * @param stage - top level JavaFX container
+     */
+    public void setInitialStageAndScene(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MainView.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add("style.css");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
--- src/main/java/com/manuco/manuco/Main.java
+++ src/main/java/com/manuco/manuco/Main.java
@@ -1,6 +1,6 @@
 package com.manuco.manuco;
 
-import com.manuco.manuco.Controller.StageController;
+import com.manuco.manuco.Controller.SceneController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,30 +15,24 @@ import java.io.IOException;
 
 public class Main extends Application {
 
-    /**
-     * Create single StageController instance to use to change scenes
-     */
-    StageController stageController = StageController.getStageManagerInstance();
+    SceneController sceneController = SceneController.getSceneControllerInstance();
 
     /**
-     * Launch point for Inventory Management System for ManuCo
+     * Launch point for JavaFX; creates main stage
      * @param stage - the top level JavaFX container for all scenes
-     * @throws - FXMLLoader.load throws I/O if fxml file is not found.
+     * @throws IOException - FXMLLoader.load throws I/O if fxml file is not found.
      */
     @Override
     public void start(Stage stage) throws IOException {
-        try {
-            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("MainView.fxml"));
-            Scene scene = new Scene(root);
-            scene.getStylesheets().add("style.css");
-            stage.setScene(scene);
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+        sceneController.setInitialStageAndScene(stage);
 
     }
 
+    /**
+     * Entry point for application
+     * @param args - arguments to launch application
+     */
     public static void main(String[] args) {
         launch();
     }
